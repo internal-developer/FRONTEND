c19b9261f1216a68786428b1b3e6634b6197eaef
ahyeon <nuyha234@gmail.com>
2024-10-10 23:59:30 +0900

    Initial commit

---
+++ service/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/mainPage/main'
+import SignUpPage from './pages/signupPage/signup'
+import UserInfoPage from './pages/userinfoPage/userinfo'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/signup' element={<SignUpPage/>}></Route>
+        <Route path='/main' element={<MainPage/>}></Route>
+        <Route path='/userinfo' element={<UserInfoPage/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ service/src/pages/userinfoPage/userinfo.js
@@ -0,0 +1,7 @@
+export default function UserInfoPage() {
+
+    return (
+        <p>Welcome to UserInfoPage!</p>
+    );
+
+}
