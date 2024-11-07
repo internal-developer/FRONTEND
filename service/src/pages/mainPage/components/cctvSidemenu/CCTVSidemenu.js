d2b299497bb56779ff530e8977ce01258de89adc
nir3y <yerinlinlyn@gmail.com>
2024-11-08 00:10:28 +0900

    feat: CCTV 사이드메뉴 내 팝업창 UI 추가 - CCTV 사이드메뉴(CCTV 이름 리스트)에 마우스 올릴 때 나타나는 팝업 UI 구조 추가 - 삭제, 수정, 추가 버튼 레이아웃

--- service/src/pages/mainPage/components/cctvSidemenu/CCTVSidemenu.js
+++ service/src/pages/mainPage/components/cctvSidemenu/CCTVSidemenu.js
@@ -1,7 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './CCTVSidemenu.scss'
 
-function CCTVSidemenu({cctvList, onCCTVSelect}) {
+function CCTVSidemenu({ cctvList, onCCTVSelect }) {
+    const [hoveredCCTV, setHoveredCCTV] = useState(null);
+    const [showPopup, setShowPopup] = useState(true);
+
+    const handleMouseEnter = (cctv) => {
+        setHoveredCCTV(cctv);
+        setShowPopup(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredCCTV(null);
+        setShowPopup(false);
+    };
+
     return (
         <div className='menu'>
             <div className='menu-title'>Menu</div>
@@ -9,12 +22,23 @@ function CCTVSidemenu({cctvList, onCCTVSelect}) {
             <div className='menu-cctvList'>
                 <ul>
                     {cctvList.map((cctv) => (
-                        <li key={cctv.id} onClick={()=> onCCTVSelect(cctv)}>
+                        <li
+                            key={cctv.id}
+                            onClick={() => onCCTVSelect(cctv)}
+                            onMouseEnter={()=>handleMouseEnter(cctv)}
+                            onMouseLeave={handleMouseLeave}
+                        >
                             {cctv.name} ({cctv.location})
+                            {hoveredCCTV === cctv && showPopup === true &&(
+                                <div className='popup'>
+                                    <div>추가</div>
+                                    <div>수정</div>
+                                    <div>삭제</div>
+                                </div>
+                            )}
                         </li>
                     ))}
                 </ul>
-
             </div>
         </div>
     )
