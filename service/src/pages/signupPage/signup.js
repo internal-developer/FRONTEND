1bd9eefe799d8298e2ff2686bd3b6e6d402f5989
youngeun Chae <dlalsgur1625@naver.com>
2024-11-12 15:30:32 +0900

    [Style] signup, userinfo scss 추가

--- service/src/pages/signupPage/signup.js
+++ service/src/pages/signupPage/signup.js
@@ -1,9 +1,15 @@
 import './signup.scss';
 
 export default function SignUpPage() {
-
     return (
-        <p>Welcome to SignUpPage!</p>
+        <div className="SignUpPage">
+            <div className="form-container">
+                <h2 className="sign-up">Sign up</h2>
+                <p className="signup-text">회원가입</p>
+                <button id="kakao-button" className="login-button"></button>
+                <button id="naver-button" className="login-button"></button>
+                <button id="google-button" className="login-button"></button>
+            </div>
+        </div>
     );
-
 }
